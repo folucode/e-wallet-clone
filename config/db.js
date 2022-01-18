3aba50744235e6a61897b2c79bf1b932b921db46
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2022-01-18 16:29:06 +0100

    connect heroku to mysql

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: 'myMySQL1234!@#$',
-    DB: 'fundall',
+    HOST: 'us-cdbr-east-05.cleardb.net',
+    USER: 'b07acaeb20ea6a',
+    PASSWORD: '25c82393',
+    DB: 'heroku_c68f8c8c1485bd7',
     dialect: 'mysql',
     pool: {
         max: 5,
